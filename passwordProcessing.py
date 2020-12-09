ede7aa7413f95c100539f6a50410e71c9528b7a5
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-12-09 15:59:07 -0600

    Day4 part 1 solved

---
+++ passwordProcessing.py
@@ -0,0 +1,24 @@
+input = open("Day4_PassportProcessing\\input.txt")
+data = input.read().splitlines()
+#print(data)
+counter = 0
+passports = []
+passports.append("")
+for x in data:
+    if len(x) != 0:
+        passports[counter] += ' ' + x
+    else:
+        passports.append("")
+        counter += 1
+    """ if counter == 2:
+        break """
+#print(passports)
+validPassports = 0
+for passport in passports:
+    fields = passport.split(' ')
+    fields.pop(0)
+    length = len(fields)
+    if length == 8 or (length == 7 and ('cid' not in passport)):
+        print(fields, length)
+        validPassports += 1
+print("Amount of valid passports: ", validPassports)
